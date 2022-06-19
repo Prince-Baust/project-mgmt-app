4e90eb9dac25b31c3deab6ec8aa34a364d406de6
Maruf Al Hossain <pmaruf42@gmail.com>
2022-06-19 17:45:26 +0600

    project-client relation built in project type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -7,9 +7,16 @@ const ProjectType = new GraphQLObjectType({
   name: 'Project',
   fields: () => ({
     id: {type: GraphQLID},
+    clientId: {type: GraphQLID},
     name: {type: GraphQLString},
     description: {type: GraphQLString},
-    status: {type: GraphQLString}
+    status: {type: GraphQLString},
+    client: {
+      type: ClientType,
+      resolve(parent, args) {
+        return clients.find(client => client.id === parent.clientId)
+      }
+    }
   })
 })
 
