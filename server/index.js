72957e248fe146ebe27a8e295f84a9ecd6e54ac9
Maruf Al Hossain <pmaruf42@gmail.com>
2022-06-19 16:33:20 +0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 4000;
+
+
+
+app.listen(port, () => console.log(`Server started at port: ${port}`));
