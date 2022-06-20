0d917c5eac280c8e285c279c77e3e8784a3f7b2a
Maruf Al Hossain <pmaruf42@gmail.com>
2022-06-20 20:51:47 +0600

    delete client from db but not in UI yet

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,6 +1,12 @@
 import {FaTrash} from 'react-icons/fa'
+import {useMutation} from '@apollo/client'
+import {DELETE_CLIENT} from "../mutation/clientMutation";
 
 const ClientRow = ({client}) => {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: {id: client.id}
+  });
+
   const {name, email, phone} = client;
   return (
     <tr>
@@ -8,7 +14,7 @@ const ClientRow = ({client}) => {
       <td>{email}</td>
       <td>{phone}</td>
       <td>
-        <button className="btn btn-danger btn-sm">
+        <button className="btn btn-danger btn-sm" onClick={deleteClient}>
           <FaTrash />
         </button>
       </td>
