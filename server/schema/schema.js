cb2de4b8ab3b120ce83037aaa108df2ead1e62d7
Maruf Al Hossain <pmaruf42@gmail.com>
2022-06-20 17:23:53 +0600

    delete project mutation complete

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -144,6 +144,14 @@ const mutation = new GraphQLObjectType({
         return project.save();
 
       }
+    },
+    //Delete a project
+    deleteProject: {
+      type: ProjectType,
+      args: {id: {type: GraphQLNonNull(GraphQLID)}},
+      resolve(parent, args) {
+        return Project.findByIdAndRemove(args.id);
+      }
     }
   }
 })
