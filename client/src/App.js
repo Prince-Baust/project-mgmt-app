c618d7106c3dfd8138cd75b05f31dad5cf10c5c6
Maruf Al Hossain <pmaruf42@gmail.com>
2022-06-20 21:19:11 +0600

    delete client update ui

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,30 @@ import Header from "./components/Header";
 import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client';
 import Clients from "./components/Clients";
 
+//TODO: deleting client/project ui update warning solution to be updated
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        Clients: {
+          merge(existing, incoming) {
+            return incoming;
+          }
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          }
+        },
+      }
+    }
+  }
+})
+
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache()
+  cache
 })
 
 
--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,10 +1,23 @@
 import {FaTrash} from 'react-icons/fa'
 import {useMutation} from '@apollo/client'
 import {DELETE_CLIENT} from "../mutation/clientMutation";
+import {GET_CLIENTS} from "../queries/clientQueries";
 
 const ClientRow = ({client}) => {
   const [deleteClient] = useMutation(DELETE_CLIENT, {
-    variables: {id: client.id}
+    variables: {id: client.id},
+    // refetchQueries: [{query: GET_CLIENTS}]       //N.B: update UI by refetching but calling refetch again n again may cause app to crash
+
+    //Updating cache to update UI is good practice
+    update(cache, {data: {deleteClient}}) {
+      const {clients} = cache.readQuery({query: GET_CLIENTS});
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: {
+          clients: clients.filter(client => client.id !== deleteClient.id)
+        }
+      })
+    }
   });
 
   const {name, email, phone} = client;
