fcb205c7d7f446dfaf8ccf33c06b074118c079e1
Maruf Al Hossain <pmaruf42@gmail.com>
2022-06-20 06:57:25 +0600

    mongodb atlas connected

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const colors = require('colors');
 require('dotenv').config();
+const connectDB = require('./config/db');
 const port = process.env.PORT || 4000;
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
 
+//Connect DB
+connectDB();
 
 app.use('/graphql', graphqlHTTP({
   schema,
