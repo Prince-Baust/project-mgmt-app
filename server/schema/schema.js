878546daed06e94246754393385dee7871aeb85d
Maruf Al Hossain <pmaruf42@gmail.com>
2022-06-20 12:35:05 +0600

    delete client mutation complete

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -95,6 +95,16 @@ const mutation = new GraphQLObjectType({
         });
         return client.save();
       }
+    },
+    //Delete a client
+    deleteClient: {
+      type: ClientType,
+      args: {
+        id: {type: GraphQLNonNull(GraphQLID)}
+      },
+      resolve(parent, args) {
+        return Client.findByIdAndRemove(args.id);
+      }
     }
   }
 })
