cf17ebb07c524fc4687082137fcefe50093a1f67
Maruf Al Hossain <pmaruf42@gmail.com>
2022-06-20 19:04:50 +0600

    loading spinner added and query moved to separate directory

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,21 +1,14 @@
-import {gql, useQuery} from '@apollo/client'
+import {useQuery} from '@apollo/client'
 import ClientRow from "./ClientRow";
+import {GET_CLIENTS} from "../queries/clientQueries";
+import Spinner from "./Spinner";
 
-const GET_CLIENTS = gql`
-  query getClients {
-    clients {
-      id
-      name
-      email
-      phone
-    }
-  }
-`
+
 
 const Clients = () => {
   const {loading, error, data} = useQuery(GET_CLIENTS);
 
-  if (loading) return <p>Loading...</p>
+  if (loading) return <Spinner />
   if (error) return <p>Opss! Something went wrong.</p>
 
   return (
